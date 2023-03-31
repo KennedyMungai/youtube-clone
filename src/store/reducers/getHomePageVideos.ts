6d8cd027bfd7af084b8649f58c4c4ed484c9643b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-31 14:54:48 +0300

    Some changes were made

--- src/store/reducers/getHomePageVideos.ts
+++ src/store/reducers/getHomePageVideos.ts
@@ -1,8 +1,23 @@
 import { createAsyncThunk } from "@reduxjs/toolkit"
+import { RootState } from ".."
+import axios from 'axios'
+
 
 const API_KEY = process.env.REACT_APP_YOUTUBE_DATA_API_KEY
 
 export const getHomePageVideos = createAsyncThunk("youtubeApp/HomePageVideos", async (isNext: boolean, { getState }) => 
 {
+    const {
+        youtubeApp: {
+            nextPageToken: nextPageTokenFromState,
+            videos
+        }
+    } = getState() as RootState;
 
+    const {
+        data: {
+            items,
+            nextPageToken
+        }
+    } = await axios.get(`${}`)
 })
