7bb160f50e5649c1bbe94bd5dae33526d14f517a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-31 14:22:59 +0300

    Created the initialState const of type initialState

---
+++ src/store/index.ts
@@ -0,0 +1,10 @@
+import { InitialState } from "../Types";
+
+const initialState: InitialState = {
+    videos: [],
+    currentlyPlaying: null,
+    searchTerm: "",
+    searchResults: [],
+    nextPageToken: null,
+    recommendedVideos: []
+}
