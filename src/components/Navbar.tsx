cc6eb495cf4967429e31da3b15b66e6b26a9ece0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-31 12:31:49 +0300

    Removed the white text styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () =>
 {
     return (
-        <div className="flex justify-between items-center px-14 h-14 bg-[#212121] opacity-95 sticky top-0 z-50 text-white">
+        <div className="flex justify-between items-center px-14 h-14 bg-[#212121] opacity-95 sticky top-0 z-50">
             <div className="flex gap-8 items-center text-2xl">
                 <div className="">
                     <GiHamburgerMenu />
