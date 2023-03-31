bb58e9431db99b8a4ddc8fbc6ee8470c942f4e84
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-31 11:52:04 +0300

    Set the Home component to the root path

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 
 function App()
@@ -7,7 +8,7 @@ function App()
   return (
     <Router>
       <Routes>
-        <Route />
+        <Route path='/' element={<Home />} />
       </Routes>
     </Router>
   )
