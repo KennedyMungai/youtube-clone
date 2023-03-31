140a2ce6c1ae1a41a14be018754d1cabd5ceab7d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-31 12:05:44 +0300

    Added a hamburger menu to the component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,11 @@ const Navbar = () =>
 {
     return (
         <div className="flex justify-between items-center px-14 h-14 bg-[#212121] opacity-95 sticky top-0 z-50">
-
+            <div className="flex gap-8 items-center text-2xl">
+                <div className="">
+                    <GiHamburgerMenu />
+                </div>
+            </div>
         </div>
     )
 }
