dc664462e45f3def062bd8fe3912bb4955f5c494
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-31 11:49:28 +0300

    Set up the Routes components for the component

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App()
 {
   return (
-    <h1 className="text-3xl font-bold underline">Hello World</h1>
-  );
+    <Router>
+      <Routes>
+        <Route />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
