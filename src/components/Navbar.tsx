5cb65b1f04afc9059e3c871931b03b4c33032881
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-31 11:59:37 +0300

    Imported some icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import { AiOutlineSearch, AiOutlineClose } from 'react-icons/ai'
+import { TiMicrophone } from 'react-icons/ti'
+import { BsYoutube, BsCameraVideo, BsBell } from 'react-icons/bs'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { IoAppsSharp } from 'react-icons/io5'
 
-type Props = {}
 
-const Navbar = (props: Props) =>
+const Navbar = () =>
 {
     return (
         <div>Navbar</div>
